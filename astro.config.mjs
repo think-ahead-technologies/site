4df9ab199376adb974053e9da4ec5b02634f6b35
Waldemar <waldemar.kindler@think-ahead.tech>
2025-02-27 11:00:30 +0100

    Upgrade to Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,84 +44,94 @@ export default defineConfig({
         }
     },
 
-    integrations: [tailwind({
-        applyBaseStyles: false
-    }), sitemap({
-        filter: sitemapIgnoreRoot,
-        i18n: {
-            defaultLocale: "de",
-            locales: {
-                en: "en",
-                de: "de" // The `defaultLocale` value must present in `locales` keys
+    integrations: [
+        tailwind({
+            applyBaseStyles: false
+        }),
+        sitemap({
+            filter: sitemapIgnoreRoot,
+            i18n: {
+                defaultLocale: "de",
+                locales: {
+                    en: "en",
+                    de: "de" // The `defaultLocale` value must present in `locales` keys
+                }
             }
-        }
-    }), mdx(), astroI18next(), icon({
-        include: {
-            tabler: ["*"],
-            "flat-color-icons": [
-                "template",
-                "gallery",
-                "approval",
-                "deployment",
-                "document",
-                "advertising",
-                "currency-exchange",
-                "voice-presentation",
-                "business-contact",
-                "settings",
-                "lock",
-                "database"
-            ],
-            "material-symbols": [
-                "account-balance-outline-rounded",
-                "cloud",
-                "construction",
-                "clinical-notes-outline",
-                "dataset-linked-rounded",
-                "deployed-code-outline",
-                "deployed-code-account-outline",
-                "deployed-code-history-outline",
-                "design-services-outline",
-                "emoji-people",
-                "euro-symbol-rounded",
-                "handshake-outline",
-                "navigate-next",
-                "person-raised-hand-outline",
-                "search-insights",
-                "settings-heart",
-                "show-chart-rounded",
-                "smartphone-outline",
-                "sunny-outline-rounded",
-                "upgrade"
-            ],
-            "simple-icons": [
-                "amazonwebservices",
-                "googlecloud",
-                "hetzner",
-                "microsoftazure",
-                "openstack",
-                "redhatopenshift",
-                "scaleway"
-            ],
-            fa: ["lightbulb-o"],
-            ph: ["graduation-cap"]
-        }
-    }), ...whenExternalScripts(() =>
-        partytown({
-            config: { forward: ["dataLayer.push"] }
-        })
-    ), compress({
-        CSS: true,
-        HTML: {
-            "html-minifier-terser": {
-                removeAttributeQuotes: false
+        }),
+        mdx(),
+        astroI18next(),
+        icon({
+            include: {
+                tabler: ["*"],
+                "flat-color-icons": [
+                    "template",
+                    "gallery",
+                    "approval",
+                    "deployment",
+                    "document",
+                    "advertising",
+                    "currency-exchange",
+                    "voice-presentation",
+                    "business-contact",
+                    "settings",
+                    "lock",
+                    "database"
+                ],
+                "material-symbols": [
+                    "account-balance-outline-rounded",
+                    "cloud",
+                    "construction",
+                    "clinical-notes-outline",
+                    "dataset-linked-rounded",
+                    "deployed-code-outline",
+                    "deployed-code-account-outline",
+                    "deployed-code-history-outline",
+                    "design-services-outline",
+                    "emoji-people",
+                    "euro-symbol-rounded",
+                    "handshake-outline",
+                    "navigate-next",
+                    "person-raised-hand-outline",
+                    "search-insights",
+                    "settings-heart",
+                    "show-chart-rounded",
+                    "smartphone-outline",
+                    "sunny-outline-rounded",
+                    "upgrade"
+                ],
+                "simple-icons": [
+                    "amazonwebservices",
+                    "googlecloud",
+                    "hetzner",
+                    "microsoftazure",
+                    "openstack",
+                    "redhatopenshift",
+                    "scaleway"
+                ],
+                fa: ["lightbulb-o"],
+                ph: ["graduation-cap"]
             }
-        },
-        Image: false,
-        JavaScript: true,
-        SVG: false,
-        Logger: 1
-    }), astrowind(), react()],
+        }),
+        ...whenExternalScripts(() =>
+            partytown({
+                config: { forward: ["dataLayer.push"] }
+            })
+        ),
+        compress({
+            CSS: true,
+            HTML: {
+                "html-minifier-terser": {
+                    removeAttributeQuotes: false
+                }
+            },
+            Image: false,
+            JavaScript: true,
+            SVG: false,
+            Logger: 1
+        }),
+        astrowind(),
+        react()
+    ],
 
     markdown: {
         remarkPlugins: [readingTimeRemarkPlugin],
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+import defaultTheme from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
+import typographyPlugin from "@tailwindcss/typography";
 
-module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
             colors: {
@@ -15,9 +17,25 @@ module.exports = {
                 sans: ["var(--aw-font-sans, ui-sans-serif)", ...defaultTheme.fontFamily.sans],
                 serif: ["var(--aw-font-serif, ui-serif)", ...defaultTheme.fontFamily.serif],
                 heading: ["var(--aw-font-heading, ui-sans-serif)", ...defaultTheme.fontFamily.sans]
+            },
+
+            animation: {
+                fade: "fadeInUp 1s both"
+            },
+
+            keyframes: {
+                fadeInUp: {
+                    "0%": { opacity: 0, transform: "translateY(2rem)" },
+                    "100%": { opacity: 1, transform: "translateY(0)" }
+                }
             }
         }
     },
-    plugins: [require("@tailwindcss/typography")],
+    plugins: [
+        typographyPlugin,
+        plugin(({ addVariant }) => {
+            addVariant("intersect", "&:not([no-intersect])");
+        })
+    ],
     darkMode: "class"
 };
